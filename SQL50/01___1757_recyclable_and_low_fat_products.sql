1326e0da933ca823150c7fb02b0b8e9159e24e5a
RomanKononovich <raman.kananovich@omegasoftware-solutions.de>
2025-03-21 18:11:55 +0300

    Add SQL50 1-2: Solutions

---
+++ SQL50/01___1757_recyclable_and_low_fat_products.sql
@@ -0,0 +1,32 @@
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | product_id  | int     |
+-- | low_fats    | enum    |
+-- | recyclable  | enum    |
+-- +-------------+---------+
+
+-- Input: 
+
+-- Products table:
+-- +-------------+----------+------------+
+-- | product_id  | low_fats | recyclable |
+-- +-------------+----------+------------+
+-- | 0           | Y        | N          |
+-- | 1           | Y        | Y          |
+-- | 2           | N        | Y          |
+-- | 3           | Y        | Y          |
+-- | 4           | N        | N          |
+-- +-------------+----------+------------+
+-- Output: 
+-- +-------------+
+-- | product_id  |
+-- +-------------+
+-- | 1           |
+-- | 3           |
+-- +-------------+
+
+--           1 
+--   | Runtime: 865ms
+--   | Beats: 68.73%
+SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable  = 'Y'
---
+++ SQL50/02___584_find_customer_referee.sql
@@ -0,0 +1,40 @@
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | referee_id  | int     |
+-- +-------------+---------+
+
+-- Input: 
+
+-- Customer table:
+-- +----+------+------------+
+-- | id | name | referee_id |
+-- +----+------+------------+
+-- | 1  | Will | null       |
+-- | 2  | Jane | null       |
+-- | 3  | Alex | 2          |
+-- | 4  | Bill | null       |
+-- | 5  | Zack | 1          |
+-- | 6  | Mark | 2          |
+-- +----+------+------------+
+-- Output: 
+-- +------+
+-- | name |
+-- +------+
+-- | Will |
+-- | Jane |
+-- | Bill |
+-- | Zack |
+-- +------+
+
+--           1 
+--   | Runtime: 629ms
+--   | Beats: 86.91%
+SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS NULL
+
+--           2 
+--   | Runtime: 2354ms
+--   | Beats 5.00%
+SELECT name FROM Customer WHERE COALESCE(referee_id, 0) != 2
