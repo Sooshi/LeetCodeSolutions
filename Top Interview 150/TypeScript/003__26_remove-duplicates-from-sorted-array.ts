f8860254bc5531adc606c6ed22783ad6da6904b5
Sooshi <pomakononovich@gmail.com>
2025-07-31 00:06:49 +0300

    Update 003

--- Top Interview 150/TypeScript/003__26_remove-duplicates-from-sorted-array.ts
+++ Top Interview 150/TypeScript/003__26_remove-duplicates-from-sorted-array.ts
@@ -1,6 +1,6 @@
 // 1
 // | Remove duplicate in place
-// | O(n * n) while, splice (cause nums.splice(i, 1), which removes one element and shifts all remaining elements one position left.)
+// | O(n * n): while, splice (cause nums.splice(i, 1), which removes one element and shifts all remaining elements one position left.)
 // | Runtime: 60ms Beats: 6.75%
 // | Memory: 58.75MB Beats: 68.20%
 function removeDuplicates_1(nums: number[]): number {
@@ -21,10 +21,28 @@ function removeDuplicates_1(nums: number[]): number {
 
 // 2
 // | Remove duplicate in place
-// | O(n) while, splice (cause nums.splice(i, 1), which removes one element and shifts all remaining elements one position left.)
+// | O(n): for
+// | Runtime: 15ms Beats: 16.16%
+// | Memory: 61.43MB Beats: 5.15%
+function removeDuplicates_2(nums: number[]): number {
+    let pointer = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i-1]) {
+            nums[pointer] = nums[i];
+            pointer++;            
+        }
+    }
+
+    return pointer;
+};
+
+// 3
+// | Remove duplicate in place
+// | O(n): while
 // | Runtime: 0ms Beats: 100.00%
 // | Memory: 58.92MB Beats: 53.36%
-function removeDuplicates_2(nums: number[]): number {
+function removeDuplicates_3(nums: number[]): number {
     let i = 1;
     let j = 1;
 
@@ -38,4 +56,22 @@ function removeDuplicates_2(nums: number[]): number {
     }
 
     return j;
+};
+
+// 4
+// | Remove duplicate in place
+// | O(n): for
+// | Runtime: 0ms Beats: 100.00%
+// | Memory: 58.80MB Beats: 53.24%
+function removeDuplicates_4(nums: number[]): number {
+    let i = 0 // index of last uniq number
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j]
+        }
+    }
+
+    return i + 1;
 };
