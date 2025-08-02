8cbdc9b88b91a43be7f72c7dbef30773a08cf472
Sooshi <pomakononovich@gmail.com>
2025-08-02 19:19:59 +0300

    Add solution 007

--- Top Interview 150/TypeScript/007__121_best-time-to-buy-and-sell-stock.ts
+++ Top Interview 150/TypeScript/007__121_best-time-to-buy-and-sell-stock.ts
@@ -1,26 +1,3 @@
-//
-// Brute force
-// Time Limit Exceeded: O(n²):
-//   O(n): for ()
-//   O(n - i): prices.slice in for
-//   O(n - i): Math.max(...arr)
-//   = O(n²)
-function maxProfit(prices: number[]): number {
-    let profit = 0;
-
-    for (let i = 0; i < prices.length; i++) {
-        const current = prices[i];
-        const arr = prices.slice(i + 1, prices.length);
-        
-        const diff = Math.max(...arr) - current;
-        if (profit < diff) {
-            profit = diff;
-        }
-    }
-
-    return profit;
-};
-
 // 1
 // | Kadane's DP algorithm (max subarray sum)
 // | Runtime: 2ms Beats: 77.54%
@@ -28,41 +5,36 @@ function maxProfit(prices: number[]): number {
 // | Memory: 65.84MB Beats: 54.27%
 // |  Spatial complexity: O(1): without using data structures that grow with input
 function maxProfit1(prices: number[]): number {
-    let maxPrice = 0;
-    let profit = 0;
+    let current = 0;
+    let max = 0;
 
-    for (let i = prices.length - 1; i >= 0; i--) {
-        maxPrice = Math.max(maxPrice, prices[i]);
-        profit = Math.max(profit, maxPrice - prices[i])
+    for (let i = 1; i < prices.length; i++) {
+        const diff = prices[i] - prices[i - 1];
+        current = Math.max(0, current + diff);
+        max = Math.max(max, current)
     }
 
-    return profit;
+    return max;
 }
 
-// Same alternative
+// 2
+// | Greedy algorithm
+// | Runtime: 1ms Beats: 95.41%
+// |  Asymptotic complexity: O(n): for ()
+// | Memory: 66.18MB Beats: 34.34%
+// |  Spatial complexity: O(1): without using data structures that grow with input
 function maxProfit2(prices: number[]): number {
-    let minPrice = Infinity;
+    let minPrice = prices[0];
     let profit = 0;
 
-    for (let price of prices) {
-        minPrice = Math.min(minPrice, price);
-        profit = Math.max(profit, price - minPrice);
-    }
-
-    return profit;
-}
-
-// Same with conditions block
-function maxProfit3(prices: number[]): number {
-    let minPrice = Infinity;
-    let profit = 0;
-
-    for (let i = 0; i < prices.length; i++) {
+    for (let i = 1; i < prices.length; i++) {
         if (prices[i] < minPrice) {
-            minPrice = prices[i]
+            minPrice = prices[i];
+            continue; // If today is the miniest price - no sense to find profit => shall continue and skip iteration ending
         }
+
         if (prices[i] - minPrice > profit) {
-            profit = prices[i] - minPrice
+            profit = prices[i] - minPrice;
         }
     }
 
