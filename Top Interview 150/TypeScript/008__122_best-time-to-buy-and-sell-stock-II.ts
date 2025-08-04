519e1520ca2d24b309c0a04235241015f9102f34
Sooshi <pomakononovich@gmail.com>
2025-08-04 19:55:15 +0300

    Add solution 010

--- Top Interview 150/TypeScript/008__122_best-time-to-buy-and-sell-stock-II.ts
+++ Top Interview 150/TypeScript/008__122_best-time-to-buy-and-sell-stock-II.ts
@@ -1,17 +1,16 @@
 // 1
-// | One array iteration
+// | 
 // | Runtime: 1ms Beats: 69.08%
 // |  Asymptotic complexity: O(n): for ()
 // | Memory: 56.36MB Beats: 43.50%
 // |  Spatial complexity: O(1): without using data structures that grow with input (using only a single variable)
-function maxProfit4(prices: number[]): number {
+function maxProfit(prices: number[]): number {
     let totalProfit = 0;
 
     for (let i = 0; i < prices.length; i++) {
-        const currentPrice = prices[i];
-
-        if (currentPrice > prices[i - 1]) {
-           totalProfit += currentPrice - prices[i - 1];
+        if (prices[i] < prices[i + 1]) {
+            totalProfit -= prices[i];
+            totalProfit += prices[i + 1];
         }
     }
 
